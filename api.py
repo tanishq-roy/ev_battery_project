c24ef557c150d8deaefc108938268629cc266424
Tanishq <tanishqr.85220@gmail.com>
2026-06-19 14:54:34 +0530

    added url

--- api.py
+++ api.py
@@ -61,7 +61,7 @@ app = FastAPI(title="EV Battery EOL Diagnostics API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://eol-diagnostics.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

fa9c6e75045530b80e9be62d6f9dbd0091205c52
Tanishq <tanishqr.85220@gmail.com>
2026-06-19 15:40:27 +0530

    Modified base url

--- api.py
+++ api.py
@@ -184,4 +184,4 @@ def predict(payload: BatteryTelemetry):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='https://eol-diagnostics.vercel.app', port=8000)

c2e781f4412b43b3b2fbd4813af4435d01f75f7e
Tanishq <tanishqr.85220@gmail.com>
2026-06-19 14:59:06 +0530

    Modified

--- api.py
+++ api.py
@@ -171,4 +171,4 @@ def predict(payload: BatteryTelemetry):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn running on http://0.0.0.0:10000
